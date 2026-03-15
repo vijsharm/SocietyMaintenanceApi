20d7fea3cda81dd7c521fab056e318dd8f1b11bd
vijit <vijit.sharma2222@yahoo.in>
2026-03-16 02:30:00 +0530

    cors config

--- src/main/java/com/society/maintenance/societymaintenance/config/ApplicationConfiguration.java
+++ src/main/java/com/society/maintenance/societymaintenance/config/ApplicationConfiguration.java
@@ -53,7 +53,7 @@ public class ApplicationConfiguration {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://nalandatown.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
